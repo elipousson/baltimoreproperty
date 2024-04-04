d14b4a8243232ef3f59787241e5fcefe28ba40b3
your name <eli.pousson@gmail.com>
2024-04-03 23:22:57 -0400

    Add VBN + HMT data

--- _targets.R
+++ _targets.R
@@ -47,6 +47,30 @@ tar_plan(
       "output",
       "baltimore_parcels.rds"
     )
+  ),
+
+  baltimore_hmt_2023 = arcgislayers::arc_read(
+    "https://geodata.baltimorecity.gov/egis/rest/services/Housing/dmxBoundaries3/MapServer/40",
+    crs = 3857
+  ),
+  baltimore_hmt_2023_out = readr::write_rds(
+    baltimore_hmt_2023,
+    here::here(
+      "output",
+      "baltimore_hmt_2023.rds"
+    )
+  ),
+
+  baltimore_vbn = arcgislayers::arc_read(
+    "https://geodata.baltimorecity.gov/egis/rest/services/Housing/dmxLandPlanning/MapServer/34",
+    crs = 3857
+  ),
+  baltimore_vbn_out = readr::write_rds(
+    baltimore_vbn,
+    here::here(
+      "output",
+      "baltimore_vbn.rds"
+    )
   )
 
 )
